8703582d0e6ae4fef7b7c374cbe8fa943d9e8696
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-12-16 01:02:28 +0100

    [Client] Set Fonts & BG-Color for projection

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -6,7 +6,8 @@
 
 <style>
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  color: white;
+  font-family: "DB Head" ,"Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -14,11 +15,20 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+
+  background-color: black;
 }
 
 *, *:before, *:after {
   box-sizing: border-box;
   margin: 0;
+}
+
+h1 {
+  font-size: 3em;
+}
+h2 {
+  font-size: 2.25em;
 }
 
 </style>
